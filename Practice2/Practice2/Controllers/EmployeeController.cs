82722e8f0ac122ca6c43447cf29d95c917a34a81
Sayali Joshi <sayalij@missionbell.com>
2021-03-14 21:52:50 -0700

    Added employee controller

---
+++ Practice2/Practice2/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practice2.Models;
+
+namespace Practice2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee emp = new Employee();
+            emp.Name = "John Doe";
+            emp.Department = "Engineering";
+            return View(emp);
+        }
+    }
+}
---
+++ Practice2/Practice2/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice2.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Department { get; set; }
+        public string Location { get; set; }
+    }
+}
